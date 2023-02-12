1c637632929a65b8ecd4919b28fe1e1b716cecfe
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-02-12 12:25:21 -0500

    Fixed Typo in dog years script name. Fallback!!

--- src/scripts/dogyears.js
+++ src/scripts/dogyears.js
@@ -1,27 +1,34 @@
-// select the element with class "result"
-const resultDiv = document.querySelector(".result");
+document
+  .getElementById("script-select")
+  .addEventListener("change", function () {
+    // Remove any existing script tags
+    const oldScripts = document.querySelectorAll("script");
+    for (let i = 0; i < oldScripts.length; i++) {
+      if (oldScripts[i].src) {
+        oldScripts[i].parentNode.removeChild(oldScripts[i]);
+      }
+    }
 
-// function to calculate the dog's age in human years
-const calculateDogAge = function () {
-  // get the current year
-  const currentYear = new Date().getFullYear();
+    // Add the new script tag
+    var selectedScript = this.value;
+    var script = document.createElement("script");
+    script.src = selectedScript;
+    document.body.appendChild(script);
+  });
+// removes previous script tags
+const currentYear = new Date().getFullYear();
+const birthYear = parseInt(prompt("Enter the birth year of the dog:"));
 
-  // prompt the user to enter the birth year of the dog
-  const birthYear = parseInt(prompt("Enter the birth year of the dog:"));
-
-  // calculate the dog's age in human years
-  let humanYears = 0;
-  if (currentYear - birthYear <= 2) {
-    // if the dog is 2 years or younger, use the first formula
-    humanYears = (currentYear - birthYear) * 10.5;
-  } else {
-    // if the dog is older than 2, use the second formula
-    humanYears = 2 * 10.5 + (currentYear - birthYear - 2) * 4;
+const calculateDogAge = function (birthYear) {
+  // The first year of a dog's life is equal to 15 human years
+  if (currentYear - birthYear <= 1) {
+    return 15 * (currentYear - birthYear);
   }
-
-  // update the innerHTML of the result div with the calculated dog's age in human years
-  resultDiv.innerHTML = `The dog is ${humanYears} years old in human years.`;
+  // Each year following equates to 9 human years
+  return 15 + 9 * (currentYear - birthYear - 1);
 };
 
-// call the function
-calculateDogAge();
+const dogAge = Math.floor(calculateDogAge(birthYear));
+
+const resultDiv = document.querySelector(".result");
+resultDiv.innerHTML = "The dog's age in human years is: " + dogAge;
