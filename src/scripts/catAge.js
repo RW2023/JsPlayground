d93f29b33ced48d81558415ad9765e8774dd8aca
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-02-12 21:10:23 -0500

    update script and CSS. Working! Fallback!

--- src/scripts/catAge.js
+++ src/scripts/catAge.js
@@ -1,22 +1,39 @@
-// Get the current year
-const currentYear = new Date().getFullYear();
+document
+  .getElementById("script-select")
+  .addEventListener("change", function () {
+    // Remove any existing script tags
+    const oldScripts = document.querySelectorAll("script");
+    for (let i = 0; i < oldScripts.length; i++) {
+      if (oldScripts[i].src) {
+        oldScripts[i].parentNode.removeChild(oldScripts[i]);
+      }
+    }
 
-// Get the birth year of the cat from the user
-const birthYear = parseInt(prompt("Enter the birth year of the cat:"));
+    // Get the birth year of the cat from the user
+    const birthYear = prompt("Enter the birth year of the cat:");
 
-// Calculate the cat's age in human years
-const calculateCatAge = function (birthYear) {
-  // The first two years of a cat's life count as 21 human years each
-  if (currentYear - birthYear <= 2) {
-    return 21 * (currentYear - birthYear);
-  }
-  // Each year following equates to 4 human years
-  return 21 + 4 * (currentYear - birthYear - 2);
-};
+    // Validate the input
+    if (!birthYear || isNaN(birthYear)) {
+      alert("Please enter a valid year.");
+      return;
+    }
 
-// Floor the result
-const catAge = Math.floor(calculateCatAge(birthYear));
+    // Calculate the cat's age in human years
+    const calculateCatAge = function (birthYear) {
+      const currentYear = new Date().getFullYear();
 
-// Display the result to a div with the class "result"
-const resultDiv = document.querySelector(".result");
-resultDiv.innerHTML = "The cat's age in human years is: " + catAge;
+      // The first two years of a cat's life count as 21 human years each
+      if (currentYear - birthYear <= 2) {
+        return 21 * (currentYear - birthYear);
+      }
+      // Each year following equates to 4 human years
+      return 21 + 4 * (currentYear - birthYear - 2);
+    };
+
+    // Floor the result
+    const catAge = Math.floor(calculateCatAge(birthYear));
+
+    // Display the result to a div with the class "result"
+    const resultDiv = document.querySelector(".result");
+    resultDiv.innerHTML = "The cat's age in human years is: " + catAge;
+  });
