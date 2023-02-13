59491252fa409d9aefe18197c71ca6779ed25418
RW2023 <121303360+RW2023@users.noreply.github.com>
2023-02-12 23:55:44 -0500

    Changed variable names from var&const to let

--- src/scripts/dogyears.js
+++ src/scripts/dogyears.js
@@ -2,7 +2,7 @@ document
   .getElementById("script-select")
   .addEventListener("change", function () {
     // Remove any existing script tags
-    const oldScripts = document.querySelectorAll("script");
+    let oldScripts = document.querySelectorAll("script");
     for (let i = 0; i < oldScripts.length; i++) {
       if (oldScripts[i].src) {
         oldScripts[i].parentNode.removeChild(oldScripts[i]);
@@ -10,16 +10,16 @@ document
     }
 
     // Add the new script tag
-    var selectedScript = this.value;
-    var script = document.createElement("script");
+    let selectedScript = this.value;
+    let script = document.createElement("script");
     script.src = selectedScript;
     document.body.appendChild(script);
   });
 // removes previous script tags
-const currentYear = new Date().getFullYear();
-const birthYear = parseInt(prompt("Enter the birth year of the dog:"));
+let currentYear = new Date().getFullYear();
+let birthYear = parseInt(prompt("Enter the birth year of the dog:"));
 
-const calculateDogAge = function (birthYear) {
+let calculateDogAge = function (birthYear) {
   // The first year of a dog's life is equal to 15 human years
   if (currentYear - birthYear <= 1) {
     return 15 * (currentYear - birthYear);
@@ -28,7 +28,7 @@ const calculateDogAge = function (birthYear) {
   return 15 + 9 * (currentYear - birthYear - 1);
 };
 
-const dogAge = Math.floor(calculateDogAge(birthYear));
+let dogAge = Math.floor(calculateDogAge(birthYear));
 
-const resultDiv = document.querySelector(".result");
+let resultDiv = document.querySelector(".result");
 resultDiv.innerHTML = "The dog's age in human years is: " + dogAge;
